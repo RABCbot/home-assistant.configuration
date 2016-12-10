50756e91cab1cb365ab4af83b6dfec5a2987c684
RABCbot <RABCbot@users.noreply.github.com>
2016-12-10 08:43:14 -0500

    Add files via upload

--- backup-ha.sh
+++ backup-ha.sh
@@ -23,7 +23,7 @@ do
   curl -T $f ftp://192.168.101.1/Stick/Padre/$f
 done
 
-curl -T ../dasher/config/config.json ftp://192.168.101.1/Stick/Padre/dasher/config.json
+curl -T /srv/homeassistant/dasher/config/config.json ftp://192.168.101.1/Stick/Padre/dasher/config.json
 
 curl -T backup-ha.sh ftp://192.168.101.1/Stick/Padre/backup-ha.sh
 
