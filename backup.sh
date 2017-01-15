b704e1e563f5aa48ea14e89cca39bd5b1b722a37
RABCbot <RABCbot@users.noreply.github.com>
2017-01-15 12:08:24 -0500

    Add files via upload

---
+++ backup.sh
@@ -0,0 +1,57 @@
+for f in *.yaml
+do
+  curl -T $f ftp://192.168.101.1/Stick/Padre/$f
+done
+
+for f in *.xml
+do
+  curl -T $f ftp://192.168.101.1/Stick/Padre/$f
+done
+ 
+for f in custom_components/*.*
+do
+  curl -T $f ftp://192.168.101.1/Stick/Padre/$f
+done
+
+for f in www/*.*
+do
+  curl -T $f ftp://192.168.101.1/Stick/Padre/$f
+done
+
+for f in panels/*.*
+do
+  curl -T $f ftp://192.168.101.1/Stick/Padre/$f
+done
+
+for f in automation/*.*
+do
+  curl -T $f ftp://192.168.101.1/Stick/Padre/$f
+done
+
+for f in lights/*.*
+do
+  curl -T $f ftp://192.168.101.1/Stick/Padre/$f
+done
+
+for f in scripts/*.*
+do
+  curl -T $f ftp://192.168.101.1/Stick/Padre/$f
+done
+
+for f in sensors/*.*
+do
+  curl -T $f ftp://192.168.101.1/Stick/Padre/$f
+done
+
+for f in switches/*.*
+do
+  curl -T $f ftp://192.168.101.1/Stick/Padre/$f
+done
+
+curl -T /srv/homeassistant/dasher/config/config.json ftp://192.168.101.1/Stick/Padre/dasher/config.json
+
+curl -T /etc/mosquitto/mosquitto.conf ftp://192.168.101.1/Stick/Padre/mosquitto/mosquitto.conf
+
+curl -T backup.sh ftp://192.168.101.1/Stick/Padre/backup.sh
+
+
--- custom_components/countdown.py
+++ custom_components/countdown.py
@@ -6,11 +6,11 @@ _LOGGER = logging.getLogger(__name__)
 DOMAIN = 'Countdown'
 
 def setup(hass, config):
-    entity_id = 'Countdown.RogueOne'
+    entity_id = 'Countdown.Christmas'
 
     def DaysLeft():
       d1 = date.today()
-      d2 = date(2016,12,16)
+      d2 = date(2016,12,25)
       d = d2.toordinal() - d1.toordinal()
       s = '%s days' % d
       return s
