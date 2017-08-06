e922d52d34e3f5bb99cb21d952494f56e228c6d2
RABCbot <RABCbot@users.noreply.github.com>
2017-08-06 07:51:42 -0400

    HASS Updated 0.50.2

--- custom_components/textbox.py
+++ custom_components/textbox.py
@@ -10,8 +10,6 @@ def setup(hass, config):
     entity_id = 'textbox.hola'
     hass.states.set(entity_id, DEFAULT_NAME)
 
-    _LOGGER.error("RB Custom Label Text!")
-
     def set_text_call(call):
         hass.states.set(entity_id, call.data.get(ATTR_NAME, DEFAULT_NAME))
 
