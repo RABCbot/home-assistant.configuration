c7dd46b9fabb0e6d04f057821a5ecac4610fa45c
RABC <RABCbot@users.noreply.github.com>
2018-05-12 11:00:27 -0400

    Add files via upload

--- backup.sh
+++ backup.sh
@@ -48,7 +48,7 @@ do
   curl -T $f ftp://admin:2putipus@192.168.101.113/stick/Padre/$f
 done
 
-curl -T /home/dasher/config.json ftp://admin:2putipus@192.168.101.113/stick/Padre/dasher/config.json
+curl -T /home/dasher/config/config.json ftp://admin:2putipus@192.168.101.113/stick/Padre/dasher/config.json
 
 curl -T /etc/mosquitto/mosquitto.conf ftp://admin:2putipus@192.168.101.113/stick/Padre/mosquitto/mosquitto.conf
 
